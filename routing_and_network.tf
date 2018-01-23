b500f050276f8ec80f515ca307a4fe77ffe5e174
Onen Julius <jonen54@gmail.com>
2018-01-24 01:35:17 +0300

    Add routing and network

---
+++ routing_and_network.tf
@@ -0,0 +1,43 @@
+# Declare the data source
+data "aws_availability_zones" "available" {}
+
+/* EXTERNAL NETWOG, IG, ROUTE TABLE */
+
+resource "aws_internet_gateway" "gw" {
+    vpc_id = "${aws_vpc.terraformmain.id}"
+    tags {
+        Name = "Internet gw terraform generated"
+    }
+}
+resource "aws_network_acl" "all" {
+    vpc_id = "${aws_vpc.terraformmain.id}"
+    egress {
+        protocol = "-1"
+        rule_no = 2
+        action = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port = 0
+        to_port = 0
+    }
+    ingress {
+        protocol = "-1"
+        rule_no = 1
+        action = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port = 0
+        to_port = 0
+    }
+    tags {
+        Name = "open acl"
+    }
+}
+resource "aws_route_table" "public" {
+    vpc_id = "${aws_vpc.terraformmain.id}"
+    tags {
+        Name = "Public"
+    }
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.gw.id}"
+    }
+}
