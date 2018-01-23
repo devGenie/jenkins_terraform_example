613816a6ba8c84961e2af7d9cf93c8f2af067ccb
Onen Julius <jonen54@gmail.com>
2018-01-24 01:03:26 +0300

    Add terraform variables

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "region" {
+    default = "us-east-2"
+}
+
+variable "vpc-fullcidr" {
+    default = "10.1.0.0/16"
+    description = "VPC cidr range"
+}
+
+variable "Subnet-public" {
+    default = "10.1.0.0/24"
+    description = "Public subnet"
+}
+
+variable "Subnet-private" {
+    default = "10.1.1.0/24"
+    description = "Private subnet"
+}
